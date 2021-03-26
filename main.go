b2d6e9f58bbbd1893e1369e1d24a1b74523dae92
krismon setiawan <krismonsetiawan57@gmail.com>
2021-03-27 02:22:07 +0700

    Looping dan kondisional

--- main.go
+++ main.go
@@ -10,6 +10,29 @@ func main() {
 	if umur >= 20 {
 		fmt.Println("Bentar lagi nikah woi....")
 	}
+
+	kalimat := "Cepat wisuda ye"
+
+	for idx, huruf := range kalimat {
+		if idx%2 == 0 {
+			fmt.Println("Huruf index genap ", string(huruf))
+		}
+	}
+
+	for idx, huruf := range kalimat {
+		hurufVokal := string(huruf)
+
+		// cara ke 1
+		/* if hurufVokal == "a" || hurufVokal == "i" || hurufVokal == "u" || hurufVokal == "e" || hurufVokal == "o" {
+			fmt.Println("Huruf vokal pada index ke-", idx)
+		} */
+
+		switch hurufVokal {
+		case "a", "i", "u", "e", "o":
+			fmt.Println("Index :", idx, "Huruf : ", hurufVokal)
+		}
+	}
+
 	fmt.Println("Hello World, nama saya ", nama)
 	fmt.Println(umur)
 }
